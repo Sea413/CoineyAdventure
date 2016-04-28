6521acedcfad6a97c4f333942e525c2a87da9ad6
Nicholas Jensen-Hay <nicholasjensenhay@gmail.com>
2016-04-28 14:41:43 -0700

    Fixed point in time. I'm sorry. I'm sooo sorry.

--- src/Spange/Controllers/HomeController.cs
+++ src/Spange/Controllers/HomeController.cs
@@ -1,16 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNet.Authorization;
+using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Mvc;
+using Spange.Models;
 
 namespace Spange.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext _context;
+        private UserManager<ApplicationUser> _userManager;
+        private SpangeDbContext _spangeContext;
+
+        public HomeController(ApplicationDbContext context, UserManager<ApplicationUser> userManager, SpangeDbContext spangeContext)
+        {
+            _context = context;
+            _userManager = userManager;
+            _spangeContext = spangeContext;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [Authorize]
+        public IActionResult Play()
+        {
+        
+            var user = _userManager.Users.FirstOrDefault(u => u.Id == User.GetUserId());
+            if (_userManager.IsInRoleAsync(user, "Player").Result)
+            {
+                return RedirectToAction("Index", "Game");
+            }
+            return View("Create");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Player player)
+        {
+            if (ModelState.IsValid)
+            {
+                player.UserId = User.GetUserId();
+                var user = _userManager.Users.FirstOrDefault(u => u.Id == player.UserId);
+                var thing = _userManager.AddToRoleAsync(user, "Player").Result;
+                _spangeContext.Players.Add(player);
+                _spangeContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(player);
         }
 
         public IActionResult About()
--- src/Spange/wwwroot/js/site.js
+++ src/Spange/wwwroot/js/site.js
@@ -1,5 +1,10 @@
 ﻿// Write your Javascript code.
-$(document).ready(function () {
+$(document).ready(function() {
     $('select').material_select();
-    $(".button-collapse").sideNav();
+    $(".button-collapse")
+        .sideNav({
+            menuWidth: 300, // Default is 240
+            edge: 'right', // Choose the horizontal origin
+            closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    });
 });
